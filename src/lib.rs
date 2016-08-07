8fdbe12b1175aab25d696f1325794849154867c1
Baptiste Covolato <b.covolato@gmail.com>
2016-08-06 18:08:05 -0700

    misc: Setup serde

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,5 @@
 //! This is the external API for store.rs
+
+#![feature(custom_derive, plugin)]
+
+#![plugin(serde_macros)]
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 //! This is the core server of store.rs
 
+#![feature(custom_derive, plugin)]
+
+#![plugin(serde_macros)]
+
 fn main() {
 
 }
