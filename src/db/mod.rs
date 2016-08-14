9a26bd75fdec2e1caf8759e0efb10636fb4b1c66
Baptiste Covolato <b.covolato@gmail.com>
2016-08-13 17:29:00 -0700

    db: Add Database type

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -1,1 +1,21 @@
 //! Actual implementation of the Database
+
+use std::collections::HashMap;
+
+pub type Data = Vec<u8>;
+
+struct Node {
+    data: Data,
+}
+
+pub struct Database {
+    data: HashMap<Data, Node>,
+}
+
+impl Database {
+    pub fn new(_root_pwd: Data) -> Self {
+        Database {
+            data: HashMap::new(),
+        }
+    }
+}
